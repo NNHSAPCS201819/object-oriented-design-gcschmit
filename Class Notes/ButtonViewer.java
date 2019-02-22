b51b65ba0c54667e68796f034e3df8407ae0db2d
Geoff Schmit <gcschmit@gmail.com>
2019-02-22 13:18:49 -0600

    initial version of ButtonViewer

---
+++ Class Notes/ButtonViewer.java
@@ -0,0 +1,50 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+/**
+ * This class demonstrates how to install an action listener.
+ *
+ * @author gcschmit
+ * @version 22 February 2019
+ */
+public class ButtonViewer
+{
+    private final int FRAME_WIDTH = 400;
+    private final int FRAME_HEIGHT = 100;
+    
+    private JFrame frame;
+    private JPanel panel;
+    private JButton button;
+    
+
+    /**
+     * Constructor for objects of class ButtonViewer
+     */
+    public ButtonViewer()
+    {
+        // 1. define and setup the UI components
+        this.frame = new JFrame();
+        this.panel = new JPanel();
+        
+        this.button = new JButton("click me");
+        this.panel.add(this.button);
+        
+        this.frame.add(this.panel);
+        
+        // 2. create listener object
+        ClickListener listener = new ClickListener();
+        
+        // 3. register listener object with component that generates events
+        this.button.addActionListener(listener);
+        
+        this.frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setVisible(true);
+    }
+    
+    public static void main(String[] args)
+    {
+        ButtonViewer viewer = new ButtonViewer();
+    }
+}
---
+++ Class Notes/ClickListener.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Write a description of class ClickListener here.
+ *
+ * @author gcschmit
+ * @version 22 February 2019
+ */
+public class ClickListener implements ActionListener
+{
+    public void actionPerformed(ActionEvent event)
+    {
+        System.out.println("button was clicked!");
+    }
+}
