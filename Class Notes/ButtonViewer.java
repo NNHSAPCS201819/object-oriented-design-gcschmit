7f04a0493570a1eef089c5dedced41bbe75536dd
Geoff Schmit <gcschmit@gmail.com>
2019-02-25 09:06:12 -0600

    made ClickListener an inner class

--- Class Notes/ButtonViewer.java
+++ Class Notes/ButtonViewer.java
@@ -15,40 +15,54 @@ public class ButtonViewer
 {
     private final int FRAME_WIDTH = 400;
     private final int FRAME_HEIGHT = 100;
-    
+
     private JFrame frame;
     private JPanel panel;
     private JLabel label;
     private JButton button;
-    
+
+    private int clickCount;
+
     public ButtonViewer()
     {
+        this.clickCount = 0;
+
         // 1. define and setup the UI components
         this.frame = new JFrame();
         this.panel = new JPanel();
-        
-        this.label = new JLabel("click the button");
+
+        this.label = new JLabel("the button was clicked " + this.clickCount +
+            " times");
         this.panel.add(this.label);
-        
-        this.button = new JButton("A");
+
+        this.button = new JButton("click me");
         this.panel.add(this.button);
-        
+
         this.frame.add(this.panel);
-        
+
         // 2. create listener object
         ClickListener listener = new ClickListener();
-        
+
         // 3. register listener object with component that generates events
         this.button.addActionListener(listener);
-        
+
         // configure the frame and show it
         this.frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setVisible(true);
     }
-    
+
     public static void main(String[] args)
     {
         ButtonViewer viewer = new ButtonViewer();
+    }
+
+    public class ClickListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {
+            clickCount++;
+            label.setText("the button was clicked " + clickCount + " times");
+        }
     }
 }
